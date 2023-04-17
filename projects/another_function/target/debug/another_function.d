c3377617d0e32454bd1b176b7418c4d188f34dfd
CodesInDarkness <cwatkinson228@gmail.com>
2023-04-17 12:24:06 -0400

    Chapter 2 and 3 - Reviewing funtions

---
+++ projects/another_function/target/debug/another_function.d
@@ -0,0 +1,1 @@
+/home/codes/Documents/GitHub/special-octo-disco/projects/another_function/target/debug/another_function: /home/codes/Documents/GitHub/special-octo-disco/projects/another_function/src/main.rs
---
+++ projects/functions/target/debug/deps/functions-2d049428bafbea1b.d
@@ -0,0 +1,5 @@
+/home/codes/Documents/GitHub/special-octo-disco/projects/functions/target/debug/deps/functions-2d049428bafbea1b: src/main.rs
+
+/home/codes/Documents/GitHub/special-octo-disco/projects/functions/target/debug/deps/functions-2d049428bafbea1b.d: src/main.rs
+
+src/main.rs:
